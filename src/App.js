1eaebda57f424a925901b8a061b59afe9d9e404a
Chris Cain <ccain@nutshell.com>
2019-06-03 21:13:07 -0400

    Add prettier, sublime proj, rc file, format

--- src/App.js
+++ src/App.js
@@ -2,11 +2,7 @@ import React from 'react';
 import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className={styles.container}>
-      Test
-    </div>
-  );
+    return <div className={styles.container}>Test</div>;
 }
 
 export default App;
